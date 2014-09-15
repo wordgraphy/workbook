0c4553a243f4198706e2b1e0b9d1fa97683c7db5
Guillermo Tirelli <me@guillermotirelli.com>
2014-09-15 20:39:41 +0200

    @wordgraphy solutions

---
+++ core/learnyounode/03-my-first-io.js
@@ -0,0 +1,10 @@
+//console.log(process.argv);
+var fs = require('fs');
+//console.log(fs.readFileSync('numbers.txt'));
+buf = fs.readFileSync(process.argv[2]);
+str = buf.toString();
+//console.log(str);
+splitted = str.split("\n");
+//console.log(splitted);
+counted = splitted.length - 1;
+console.log(counted);
